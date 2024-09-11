60abf9454c4c1becee3dfbeb0cce15043c3a5a14
Tasya Khaerani Janubiya <90681730+janubiyyy@users.noreply.github.com>
2024-09-11 11:46:09 +0700

    haha

---
+++ app/page.jsx
@@ -0,0 +1,50 @@
+import Photo from "@/components/Photo";
+import Social from "@/components/Social";
+import Stats from "@/components/Stats";
+import { Button } from "@/components/ui/button";
+import { FiDownload } from "react-icons/fi";
+
+const Home = () => {
+  return (
+    <section className="h-full">
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+          <div className="text-center xl:text-left order-2 xl:order-none">
+            <span className="text-xl">Software Developer</span>
+            <h1 className="h1 mb-6">
+              Hello I'm <br />{" "}
+              <span className="text-accent">Tasya Khaerani Janubiya</span>
+            </h1>
+            <p className="max-w-[500px] mb-9 text-white/80">
+            I am Tasya Khaerani Janubiya, a graduate of Information Systems from Universitas Nasional with over two years of experience in web development. I specialize in React JS, Vue JS, Nuxt JS, and full-stack development with Express.js, Node.js, and MySQL. I’ve successfully launched several websites praised for their efficiency and user-friendly design. I am eager to contribute as a Front End Developer or Full Stack, with a focus on UI/UX and Agile methodologies.
+            </p>
+            <div className="flex flex-col xl:flex-row items-center gap-8">
+              <a href="/assets/cvax.pdf" download>
+                <Button
+                  variant="outline"
+                  size="lg"
+                  className="uppercase flex items-center gap-2 "
+                >
+                  <span>Download CV</span>
+                  <FiDownload className="text-xl" />
+                </Button>
+              </a>
+              <div className="mb-8 xl:mb-0">
+                <Social
+                  containerStyles="flex gap-6"
+                  iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="order-1 xl:order-none mb-8 xl:mb-0">
+            <Photo />
+          </div>
+        </div>
+      </div>
+      <Stats />
+    </section>
+  );
+};
+
+export default Home;
---
+++ app/project/page.jsx
@@ -0,0 +1,179 @@
+"use client";
+
+import { motion } from "framer-motion";
+import React, { useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import { BsArrowUpRight, BsGithub } from "react-icons/bs";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import Link from "next/link";
+import Image from "next/image";
+import WorkSliderBtns from "@/components/WorkSliderBtns";
+
+const projects = [
+  {
+    num: "01",
+    category: "Website Jaja ID ",
+    title: "Jaja ID",
+    description:
+      "Jaja.id is one of the first Hobby Marketplaces in Indonesia which provides buying and selling facilities from consumer to consumer. Everyone can open an online shop at Jaja.id and serve buyers from all over Indonesia to help fulfill the hobby needs of the Indonesian people.",
+    stack: [
+      { name: "Vue js" },
+      { name: "Vuex" },
+      { name: "Flickity" },
+      { name: "Boostrap Vue" },
+      { name: "Firebase" },
+    ],
+    image: "/assets/work/jajaid.png",
+    live: "https://jaja.id/",
+    github: "",
+  },
+  {
+    num: "02",
+    category: "Website Jaja Auto ",
+    title: "Jaja Auto",
+    description:
+      "Jaja Auto is a company in the Automotive Sales sector that facilitates New Car, Used Car, Trade in. We strive to provide the best quality service, with a wide selection of quality cars for you",
+    stack: [
+      { name: "Vue Js" },
+      { name: "Vuetify" },
+      { name: "Flickity" },
+      { name: "Boostrap Vue" },
+    ],
+    image: "/assets/work/jatoo.png",
+    live: "https://auto.jaja.id/",
+    github: "",
+  },
+  {
+    num: "03",
+    category: "Website Eureka Book House",
+    title: "Eureka Book House",
+    description:
+      "We make it easy for you to buy all products related to Books, ATK, Lifestyle, Toys & Hobbies, Sports & Outdoors and Handmade, just by accessing the Eurekabookhouse.co.id site.",
+    stack: [{ name: "Vue" }, { name: "Vuex" }, { name: "Vue Boostrap" }, { name: "Flickity" }, { name: "Lodash" }],
+    image: "/assets/work/ebhhh.png",
+    live: "https://sandbox.eurekabookhouse.co.id/",
+    github: "",
+  },
+  {
+    num: "04",
+    category: "Website CMS Katarasa",
+    title: "Katarasa",
+    description:
+      "website administrator to manage and monitor systems or applications. It provides an overall view of important data and statistics, as well as tools for performing administrative actions.",
+    stack: [{ name: "React" }, { name: "Antd" }, { name: "Tailwind" }, { name: "Vite" }],
+    image: "/assets/work/katras.png",
+    live: "https://sandbox.eurekabookhouse.co.id/",
+    github: "",
+  },
+];
+
+const Project = () => {
+  const [project, setProject] = useState(projects[0]);
+
+  const handleSlideChange = (swiper) => {
+    const currentIndex = swiper.activeIndex;
+    setProject(projects[currentIndex]);
+  };
+  return (
+    <motion.section
+      initial={{
+        opacity: 0,
+        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+      }}
+      animate={{ opacity: 1 }}
+      className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0"
+    >
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row xl:gap-[30px]">
+          <div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
+            <div className="flex flex-col gap-[30px] h-[50%]">
+              <div className="text-8xl leading-none font-extrabold text-transparent text-outline">
+                {project.num}
+              </div>
+              <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500 capitalize">
+                {project.category} project
+              </h2>
+              <p className="text-white/60">{project.description}</p>
+              <ul className="flex gap-4">
+                {project.stack.map((item, index) => {
+                  return (
+                    <li key={index} className="text-xl text-accent">
+                      {item.name}
+                      {index !== project.stack.length - 1 && ","}
+                    </li>
+                  );
+                })}
+              </ul>
+              <div className="border border-white/20"></div>
+              <div className="flex items-center gap-4">
+                <Link href={project.live}>
+                  <TooltipProvider delayDuration={100}>
+                    <Tooltip>
+                      <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
+                        <BsArrowUpRight className="text-white text-3xl group-hover:text-accent" />
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        <p>Live Project</p>
+                      </TooltipContent>
+                    </Tooltip>
+                  </TooltipProvider>
+                </Link>
+                {/* <Link href={project.github}>
+                  <TooltipProvider delayDuration={100}>
+                    <Tooltip>
+                      <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
+                        <BsGithub className="text-white text-3xl group-hover:text-accent" />
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        <p>Github Repository</p>
+                      </TooltipContent>
+                    </Tooltip>
+                  </TooltipProvider>
+                </Link> */}
+              </div>
+            </div>
+          </div>
+          <div className="w-full xl:w-[50%] ">
+            <Swiper
+              spaceBetween={30}
+              slidesPerView={1}
+              className="xl:h-[520px] mb-12"
+              onSlideChange={handleSlideChange}
+            >
+              {projects.map((project, index) => {
+                return (
+                  <SwiperSlide key={index} className="w-full">
+                    <div className="h-[365px] relative group flex justify-center items-center bg-transparent">
+                      <div className="absolute top-0 bottom-0 w-full h-full bg-black/10 z-10"></div>
+                      <div className="relative w-full h-full">
+                        <Image
+                          src={project.image}
+                          fill
+                          className="xl:object-contain sm:object-cover md:object-contain"
+                          alt=""
+                        />
+                      </div>
+                    </div>
+                  </SwiperSlide>
+                );
+              })}
+              <WorkSliderBtns
+                containerStyles="flex gap-2 absolute right-0 bottom-[calc(50%_-_22px)] xl:bottom-0 z-20 w-full justify-between xl:w-max xl:justify-none"
+                btnStyles="bg-accent hover:bg-accent-hover text-primary text-[22px] w-[44px] h-[44px] flex justify-center items-center transition-all"
+                iconStyles
+              />
+            </Swiper>
+          </div>
+        </div>
+      </div>
+    </motion.section>
+  );
+};
+
+export default Project;
